e58743d050529f94afa8f9aae427dc4234e9be97
oldixi <oldixi@yandex.ru>
2025-05-24 23:34:42 +0300

    feat: add maven and db

---
+++ create_tables.sql
@@ -0,0 +1,15 @@
+select version();
+drop table post;
+create table post(
+    id bigserial primary key,
+    name varchar(250) not null,
+    picture bytea,
+    post_text text,
+    tags varchar(4000),
+    likes_count integer default 0);
+drop table comment;
+create table comment(
+    id bigserial primary key,
+    comment_text text,
+    post_id bigint,
+    foreign key (post_id) references post (id) on delete cascade);
